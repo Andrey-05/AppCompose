3a2bf993a9a2de53a7c18b12238307c2f1fb60f5
andrey <andreylrodrigues@hotmail.com>
2022-11-28 23:53:41 -0300

    exercicio2

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,17 +22,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //conteudo que sera mostardo ao rodar
         setContent {
-            Text("Teste do Compose")
-            MessageCard("Andrey o tal")
+            MessageCard(Message("Andrey o Tal", "Rei do kotlin não tem jeito"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
 
 //função que renderiza o card de menssagem
-fun MessageCard(name: String) {
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message) {
+    //Alinha todos os elementos de dentro em linha
+    Row(
+        //Cria espaço de todos os lados do card
+        modifier = Modifier.padding(all = 8.dp)
+    ) {
+
+        //icone do do usuário
+        Image(
+            //seleciona a imagem que sera usada
+            painter = painterResource(R.drawable.senhor),
+            //texto para caso imagem não carrege
+            contentDescription = "Icone do usuário",
+            //Define estilo do imagem
+            modifier = Modifier
+                .size(45.dp)
+                .clip(CircleShape)
+        )
+
+        //Separa os elementos horizontalmente
+        Spacer(modifier = Modifier
+            .width(8.dp))
+
+        //alinha elementos de dentro em coluna
+        Column {
+            //texto com nome do usuário
+            Text(text = msg.author)
+
+            //Separa os elementos verticalmente
+            Spacer(modifier = Modifier
+                .height(4.dp))
+
+            //texto de descrição
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
@@ -35,5 +76,5 @@ fun MessageCard(name: String) {
 //preview para testes
 fun PreviewMessageCard() {
     val name = "preview"
-    Text(text = "Hello $name")
+    MessageCard(Message("Andrey o Tal", "Rei do kotlin não tem jeito"))
 }
